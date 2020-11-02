8d2a275a15181aadeae38b0266d68ca712492719
Thor Hlavaty <thorthebore@gmail.com>
2020-11-02 13:02:20 -0500

    This looks awful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./styles.css";
+import Stepper from "./Stepper.js";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Counter</h1>
+      <Stepper />
     </div>
   );
 }
-
-export default App;
